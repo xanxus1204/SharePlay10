29c5995748551b1681a5e722bd6043bc84b862a4
suguru <icloud_i4s.com>
2016-09-29 11:58:38 +0900

    Initial Commit

---
+++ SharePlay2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SharePlay2
+//
+//  Created by 椛島優 on 2016/09/29.
+//  Copyright © 2016年 椛島優. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
