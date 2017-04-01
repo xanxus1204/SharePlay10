daa67935a5dfe75eff8a2a47b8f2541cf37a0676
椛島優 <suguru@kabashimayuu-no-MacBook-Air.local>
2017-04-01 18:34:09 +0900

    staging環境追加

---
+++ SharePlay2/AudioSessionManager.swift
@@ -0,0 +1,13 @@
+//
+//  AudioSessionManager.swift
+//  SharePlay2
+//
+//  Created by 椛島優 on 2017/04/01.
+//  Copyright © 2017年 椛島優. All rights reserved.
+//
+
+import UIKit
+
+class AudioSessionManager: NSObject {
+
+}
